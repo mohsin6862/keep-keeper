cb9d1684cc280dc36d54d8aae53cda680cdd1cd7
mohsin6862 <mohsinctg2206@gmail.com>
2026-04-15 15:43:19 +0600

    Footer added with styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <div></div>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      
+    </div>
   );
 }
